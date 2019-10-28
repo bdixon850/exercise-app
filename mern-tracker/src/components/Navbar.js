f1391ff77022b87e665c32b8caa9a1bbaec4cfe5
Brandon Dixon <bdixon850@yahoo.com>
2019-10-28 16:56:16 -0400

    adding frontend

---
+++ mern-tracker/src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+                <Link to="/" className="navbar-brand">ExcerTracker</Link>
+                <div className="collapse navbar-collapse">
+                <ul className="navbar-nav mr-auto">
+                    <li className="navbar-item">
+                    <Link to="/" className="nav-link">Exercises</Link>
+                    </li>
+                    <li>
+                        <Link to="/create" className="nav-link">Create Exerciese Log</Link>
+                    </li>
+                </ul>
+                </div>
+            </nav>
+        )
+    }
+}
---
+++ mern-tracker/src/components/index.js
@@ -0,0 +1,4 @@
+export { default as CreateExercise } from './CreateExercise';
+export { default as CreateUser } from './CreateUser';
+export { default as EditExercise } from './EditExercise';
+export { default as ExerciseList } from './ExerciseList';
